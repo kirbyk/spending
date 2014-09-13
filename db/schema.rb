f8877717a389577a502f107e9814347103754d57
Spencer Brown <me@spencer.sx>
2014-09-13 04:03:07 -0400

    add access token to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140913074136) do
+ActiveRecord::Schema.define(version: 20140913080109) do
 
   create_table "tags", force: true do |t|
     t.integer  "user_id"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20140913074136) do
     t.datetime "updated_at"
     t.string   "provider"
     t.string   "uid"
+    t.string   "access_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
