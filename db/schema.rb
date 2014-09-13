ff801fa046c66ab1024dd8e1685ba7331bcabe92
Scott Opell <me@scottopell.com>
2014-09-13 02:38:07 -0400

    adds tags and relation to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140913061446) do
+ActiveRecord::Schema.define(version: 20140913062653) do
+
+  create_table "tags", force: true do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "tags", ["user_id"], name: "index_tags_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
