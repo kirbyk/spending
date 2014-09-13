20b20f187eb6f9bfd9de700b13b40fedf1a33ced
Spencer Brown <me@spencer.sx>
2014-09-13 02:50:14 -0400

    AddColumnsToUsers provider uid

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140913062653) do
+ActiveRecord::Schema.define(version: 20140913064949) do
 
   create_table "tags", force: true do |t|
     t.integer  "user_id"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 20140913062653) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
