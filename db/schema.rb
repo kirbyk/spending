2e0ff6e8d6a790021d83332dad0ea12b5f4505c2
Spencer Brown <me@spencer.sx>
2014-09-13 03:45:55 -0400

    allow user email to be null

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140913064949) do
+ActiveRecord::Schema.define(version: 20140913074136) do
 
   create_table "tags", force: true do |t|
     t.integer  "user_id"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20140913064949) do
   add_index "tags", ["user_id"], name: "index_tags_on_user_id"
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
+    t.string   "email",                  default: ""
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
