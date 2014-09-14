aeaa0495cd1a2913ca14bfe0b7240b9fbfee02ec
Kirby Kohlmorgen <kirby@purdue.edu>
2014-09-13 22:03:13 -0400

    Remove transactions from routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root to: 'home#splash'
   get '/dashboard', to: 'home#dashboard'
-  get '/transactions', to: 'home#transactions'
   get 'mfa_new', to: 'home#mfa_new'
   post '/bank_create', to: 'home#bank_create'
   post '/mfa_save', to: 'home#mfa_save'
