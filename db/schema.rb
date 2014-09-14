ec7e5069d89963add6557a3479cf4566fb6a5b32
Scott Opell <me@scottopell.com>
2014-09-13 21:03:55 -0400

    initial commit for tags

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140914004517) do
+ActiveRecord::Schema.define(version: 20140914010257) do
 
   create_table "categories", force: true do |t|
     t.string   "type"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20140914004517) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "category_id"
   end
 
   add_index "tags", ["user_id"], name: "index_tags_on_user_id"
